2f4502efab52b604a2f67d4c556ccbe0c9252dcc
Ali Sabil <ali.sabil@soundrop.com>
2013-02-20 16:46:26 +0100

    Add mixpanel_app:start/0 for testing purposes

--- src/mixpanel_app.erl
+++ src/mixpanel_app.erl
@@ -1,4 +1,7 @@
 -module(mixpanel_app).
+-export([
+	start/0
+]).
 
 -behaviour(application).
 -export([
@@ -6,9 +9,24 @@
 	stop/1
 ]).
 
+start() ->
+	ensure_started(mixpanel).
+
 
 start(_StartType, _StartArgs) ->
 	mixpanel_sup:start_link().
 
 stop(_State) ->
 	ok.
+
+%% @private
+ensure_started(App) ->
+	case application:start(App) of
+		ok ->
+			true;
+		{error, {already_started, App}} ->
+			true;
+		{error, {not_started, Other}} ->
+			ensure_started(Other),
+			ensure_started(App)
+	end.
